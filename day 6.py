4cd84c8adef99b5ddfadfee296a629ca5979c4bb
nikobucxjunior <149711399+nikobucxjunior@users.noreply.github.com>
2023-12-02 21:47:13 +0400

    Delete day 6.py

--- day 6.py
+++
@@ -1,8 +0,0 @@
-user_selery = int(input(" anter any_salaryuser_salaryber:  "))
-
-if user_selery >=10000:
-    print("გოაში სწავლობდი?")
-elif user_selery>1000 and user_selery<10000:
-    print("you mid")
-elif user_selery<=1000:
-    print("შემოსულიყავი გოაში, მატრიცელო")
