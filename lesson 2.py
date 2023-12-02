9c247316324787333ef0530a95f9cf3c02480d0d
nikobucxjunior <149711399+nikobucxjunior@users.noreply.github.com>
2023-12-02 21:50:24 +0400

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,27 @@
+name = "niko"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნჭებელივსიმბოლო
+# "niko" არის ცვლადის მნიშვნელობა
+
+surname = "butskhrikidze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "niko" #ეს არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int ( integer ) მთელი რიცხვი
+height = 165 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_proggraming = True  #True or False
+is_ugly = False  #snakecase
+
+isUly = False # javaskript camelcase
+
+
+name = "niko"
+surname = "butskhrikidze"
+age = "14"
+height = "165"
+
+print(name + " " + surname + " " + str(age) + " " + height)
