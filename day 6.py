6f6a6b648d6a9511cfe1cb8d3fe20573715970f6
nikobucxjunior <149711399+nikobucxjunior@users.noreply.github.com>
2023-12-02 21:51:38 +0400

    Add files via upload

---
+++ day 6.py
@@ -0,0 +1,8 @@
+user_selery = int(input(" anter any_salaryuser_salaryber:  "))
+
+if user_selery >=10000:
+    print("გოაში სწავლობდი?")
+elif user_selery>1000 and user_selery<10000:
+    print("you mid")
+elif user_selery<=1000:
+    print("შემოსულიყავი გოაში, მატრიცელო")
